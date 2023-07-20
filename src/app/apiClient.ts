a57c04e5cc566298af3e027513a85f7a5a3e1e1e
Robin <robin@lemonhive.com>
2023-07-20 16:39:53 +0600

    api base url updated

--- src/app/apiClient.ts
+++ src/app/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://ebazar.cyclic.app/api",
 });
 
 export default apiClient;
